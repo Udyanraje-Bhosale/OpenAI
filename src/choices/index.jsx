a4f180e8558ed48c8dea339fae0a895c8d00fbc9
Udyanraje-Bhosale <amar10bhosale7@gmail.com>
2023-07-14 07:57:22 +0530

    second commit

--- src/choices/index.jsx
+++ src/choices/index.jsx
@@ -17,31 +17,72 @@ export const items = [
         name: 'Grammer Correction',
         id: 'gc',
         description: 'Corrects sentences into standard English.',
+        option: {
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
 
     },
     {
         name: 'English to other languages',
         id: 'trans',
         description: 'Translates English text into French, Spanish and Japanese.',
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.3,
+            max_tokens: 100,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+        }
 
     },
     {
         name: 'Movie to Emoji',
         id: 'm2e',
         description: 'Convert movie titles into emojiimage.',
+        option: {
+            model: "text-davinci-003",
+            temperature: 0.8,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+            stop: ["\n"],
+        }
 
     },
     {
         name: 'Explain code',
         id: 'code',
         description: 'Explain a complicated piece of code.',
+        option:{
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 150,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+            stop: ["\"\"\""],
+          }
 
     },
     {
         name: 'JavaScript to Python',
         id: 'j2p',
         description: 'Convert simple JavaScript expressions into Python.',
-
+        option:{
+            model: "text-davinci-003",
+            temperature: 0,
+            max_tokens: 64,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+          }
     },
 
 
